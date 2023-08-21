4ae5c5324e0b06d3249a1171494090767f5cde97
Victor Poletti <victorpoletti2009@hotmail.com>
2023-08-21 06:52:15 -0300

    feat: criando rota GET de todos os livros

---
+++ src/pages/api/books/index.api.ts
@@ -0,0 +1,44 @@
+import { prisma } from '@/src/lib/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PAGE_SIZE } from '../constants';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
+
+  const paginationCursorId = String(req.query.cursorId || '');
+
+  let bookList = [];
+
+  if (paginationCursorId) {
+    bookList = await prisma.book.findMany({
+      take: PAGE_SIZE,
+      skip: 1,
+      cursor: {
+        id: paginationCursorId,
+      },
+      orderBy: {
+        id: 'desc',
+      },
+    });
+  } else {
+    bookList = await prisma.book.findMany({
+      take: PAGE_SIZE,
+      orderBy: {
+        id: 'desc',
+      },
+    });
+  }
+
+  let newPaginationCursorId = '';
+  if (bookList.length > 0) {
+    const lastBookInList = bookList.at(-1);
+    newPaginationCursorId = String(lastBookInList?.id);
+  }
+
+  return res.json({ books: bookList, cursorId: newPaginationCursorId });
+}
