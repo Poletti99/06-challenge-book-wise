d7158aa3206b70b97959bace71c6fcfcd1df84fe
Victor Poletti <victorpoletti2009@hotmail.com>
2023-08-26 20:42:58 -0300

    feat: getStaticProps na pagina de explore

--- src/pages/explore/index.page.tsx
+++ src/pages/explore/index.page.tsx
@@ -1,31 +1,21 @@
 import { Heading } from '@/src/components/Heading';
 import { PageContainer } from '@/src/components/PageContainer';
 import { api } from '@/src/lib/axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Book } from './components/Book';
 import { ExploreBookList, ExploreContainer } from './styles';
 import { BookT } from './types';
+import { GetStaticProps } from 'next';
 
 interface GETBooksAxiosResponse {
   books: BookT[];
   cursorId: string;
 }
 
-export default function Explore() {
-  const [booksToExplore, setBooksToExplore] = useState<BookT[]>([]);
-  const [cursorId, setCursorId] = useState('');
-
-  useEffect(() => {
-    api
-      .get<GETBooksAxiosResponse>('/books')
-      .then(({ data }) => {
-        setBooksToExplore(data.books);
-        setCursorId(data.cursorId);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+interface ExploreProps extends GETBooksAxiosResponse {}
+export default function Explore({ books, cursorId }: ExploreProps) {
+  const [booksToExplore, setBooksToExplore] = useState<BookT[]>(books);
+  const [currentCursorId, setCurrentCursorId] = useState(cursorId);
 
   return (
     <PageContainer>
@@ -48,3 +38,16 @@ export default function Explore() {
     </PageContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const {
+    data: { books, cursorId },
+  } = await api.get<GETBooksAxiosResponse>('/books');
+
+  return {
+    props: {
+      books,
+      cursorId,
+    },
+  };
+};
